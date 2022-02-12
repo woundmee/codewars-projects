c84fbd0e5bb648d653ff80632d9ad64981b4a6f3
woundmee <woundmee@gmail.com>
2022-02-12 22:58:55 +0700

    Первый коммист пустого NUnit-проекта в VS 22 (MacOS)

---
+++ CodeWarsTests/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+
+namespace CodeWarsTests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
